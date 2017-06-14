8c1c8304b79692e333d18b437eccd33cc69d5d0e
admin <admin>
2017-06-14 21:51:12 +0800

    我要搞事情

--- NBMaket/src/main/java/com/nbteam/nbjdmaket/view/activity/MainActivity.java
+++ NBMaket/src/main/java/com/nbteam/nbjdmaket/view/activity/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.print("Robin 1111111111111111111");
         System.out.print("Robin 2222222222222222222");
         System.out.print("zw 我是一个粉刷匠");
+        System.out.print("zww 我要搞事情");
         System.out.print("zw 我是一个粉刷匠444444444444");
         System.out.print("zw 我是一个粉刷匠555");
 
