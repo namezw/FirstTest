23e39522e9af731b95772685456341d68d83b275
AndroidGOGO <805571245@qq.com>
2017-06-14 20:25:59 +0800

    搭建框架测试

---
+++ NBMaket/src/main/java/com/nbteam/nbjdmaket/util/Contant.java
@@ -0,0 +1,9 @@
+package com.nbteam.nbjdmaket.util;
+
+/**
+ * Created by ${廖昭启} on 2017/6/14.
+ */
+
+public class Contant {
+    public  static  final String HOST="http://139.199.157.18/market/home";
+}
--- NBMaket/src/main/java/com/nbteam/nbjdmaket/view/MainActivity.java
+++ NBMaket/src/main/java/com/nbteam/nbjdmaket/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nbteam.nbjdmaket;
+package com.nbteam.nbjdmaket.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nbteam.nbjdmaket.R;
 
 public class MainActivity extends AppCompatActivity {
 
