5409131db07abced1061515ba0014498d6a6f4a7
cz <840264786@qq.com>
2017-06-14 20:20:01 +0800

    Robin 444444444444444

--- NBMaket/src/main/java/com/nbteam/nbjdmaket/MainActivity.java
+++ NBMaket/src/main/java/com/nbteam/nbjdmaket/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.print("Robin 1111111111111111111");
         System.out.print("Robin 2222222222222222222");
         System.out.print("zw 我是一个粉刷匠");
+        System.out.print("zw 我是一个粉刷匠444444444444");
+
     }
 }
