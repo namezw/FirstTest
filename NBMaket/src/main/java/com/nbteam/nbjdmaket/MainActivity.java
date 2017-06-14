2886614e4a06b8810909f5d8f9ccab3bfa9391a4
cz <840264786@qq.com>
2017-06-14 20:33:59 +0800

    5555

--- NBMaket/src/main/java/com/nbteam/nbjdmaket/MainActivity.java
+++ NBMaket/src/main/java/com/nbteam/nbjdmaket/MainActivity.java
@@ -1,7 +1,7 @@
 package com.nbteam.nbjdmaket;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
