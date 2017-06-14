87af63e0048aa47776394207a8b759349ebe5ccb
williamlaw99 <williamlaw99@sohu.com>
2017-06-14 20:36:18 +0800

    commit by 罗树威 again

--- NBMaket/src/main/java/com/nbteam/nbjdmaket/MainActivity.java
+++ NBMaket/src/main/java/com/nbteam/nbjdmaket/MainActivity.java
@@ -1,7 +1,8 @@
 package com.nbteam.nbjdmaket;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +14,13 @@ public class MainActivity extends AppCompatActivity {
         System.out.print("Robin 2222222222222222222");
         System.out.print("zw 我是一个粉刷匠");
         System.out.print("zw 我是一个粉刷匠444444444444");
+        System.out.print("zw 不是粉刷匠，是逗逼11111");
+
 
         init();
     }
 
     private void init() {
         Log.d("Main","LSW");
-        Log.d("Main","LSW again");
-
     }
 }
